a9225af87eceb41d34d8de0d92a6bc87a427c4b5
PhoXinuS <piotr.szkoda.mail@gmail.com>
2026-05-10 22:25:36 +0200

    cleaned the original no_sleep code

--- no_sleep.py
+++ no_sleep.py
@@ -1,49 +1,46 @@
 #!/usr/bin/env python3
 
-from time import sleep
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B
+from ev3dev2.sensor import INPUT_2, INPUT_3
+from ev3dev2.sensor.lego import ColorSensor
 
-from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
-
-from ev3dev2.sensor import INPUT_1, INPUT_2,INPUT_3
-from ev3dev2.sensor.lego import TouchSensor, ColorSensor
-
-# myli black jako blue
-#wykrywa dobrze red, yellow, blue tak sobie 
+SPEED = -6
+BACKGROUND_COLOR = 'White'
+LINE_COLOR = 'Black'
 
 m_right = LargeMotor(OUTPUT_A)
-m_left = LargeMotor(OUTPUT_B)
-lift = MediumMotor(OUTPUT_C)
+m_left  = LargeMotor(OUTPUT_B)
 
 s_right = ColorSensor(INPUT_2)
-s_left = ColorSensor(INPUT_3)
-speed = -6
-m_right.off()
-m_left.off()
-lift.on_for_seconds(SpeedRPM(200), 5)
+s_left  = ColorSensor(INPUT_3)
+
+print("Line follower started")
+
 while True:
-	print('Color 1 ' + str(s_right.rgb) + ' detected as ' + str(s_right.color_name) + '.')
-	print('Color 2 ' + str(s_left.rgb) + ' detected as ' + str(s_left.color_name) + '.')
-	if (str(s_right.color_name) == "White" and str(s_left.color_name) == "White"):
-		print('forward')
-		m_right.on(speed)
-		m_left.on(speed)
-	if (str(s_right.color_name) != "White" and str(s_left.color_name) != "White"):
-		print('forward_crossing')
-		m_right.on(speed)
-		m_left.on(speed)
-	if (str(s_right.color_name) != "White" and str(s_left.color_name) == "White"):
-		print('right_turn')
-		m_right.on(speed*-1)
-		m_left.on(speed*-1)
-		m_right.on(speed*-1)
-		m_left.on(speed)
-		turn = 0
-	if (str(s_right.color_name) == "White" and str(s_left.color_name) != "White"):
-		print('left_turn')
-		m_right.on(speed*-1)
-		m_left.on(speed*-1)
-		m_right.on(speed)
-		m_left.on(speed*-1)
-		turn = 0
+    right = s_right.color_name
+    left  = s_left.color_name
+    print(f"Right: {right}, Left: {left}")
 
+    if right == BACKGROUND_COLOR and left == BACKGROUND_COLOR:
+        print('forward')
+        m_right.on(SPEED)
+        m_left.on(SPEED)
 
+    elif right != BACKGROUND_COLOR and left != BACKGROUND_COLOR:
+        print('forward_crossing')
+        m_right.on(SPEED)
+        m_left.on(SPEED)
+
+    elif right != BACKGROUND_COLOR and left == BACKGROUND_COLOR:
+        print('right_turn')
+        m_right.on(SPEED * -1)
+        m_left.on(SPEED * -1)
+        m_right.on(SPEED * -1)
+        m_left.on(SPEED)
+
+    elif right == BACKGROUND_COLOR and left != BACKGROUND_COLOR:
+        print('left_turn')
+        m_right.on(SPEED * -1)
+        m_left.on(SPEED * -1)
+        m_right.on(SPEED)
+        m_left.on(SPEED * -1)
